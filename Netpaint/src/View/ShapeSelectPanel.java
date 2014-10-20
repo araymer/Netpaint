acaefa49012c5b9e4b09bb234d5580354c47429e
araymer <aaronraymer26@gmail.com>
2014-10-19 19:06:29 -0700

    adding

--- Netpaint/src/View/ShapeSelectPanel.java
+++ Netpaint/src/View/ShapeSelectPanel.java
@@ -3,8 +3,11 @@ package View;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+
+import Model.SelectedShape;
 
 public class ShapeSelectPanel extends JPanel {
 	
@@ -12,9 +15,12 @@ public class ShapeSelectPanel extends JPanel {
 	private JRadioButton ellipseBtn;
 	private JRadioButton rectBtn;
 	private JRadioButton imageBtn;
+	private ButtonGroup group;
+	private SelectedShape current;
 	
 	public ShapeSelectPanel() {
 		
+		group = new ButtonGroup();
 		
 		lineBtn = new JRadioButton();
 		rectBtn = new JRadioButton();
@@ -29,6 +35,11 @@ public class ShapeSelectPanel extends JPanel {
 		this.setPreferredSize(new Dimension(800, 50));
 		this.setLayout(new FlowLayout());
 		
+		
+		group.add(lineBtn);
+		group.add(ellipseBtn);
+		group.add(rectBtn);
+		group.add(imageBtn);
 		
 		this.add(lineBtn);
 		this.add(ellipseBtn);
